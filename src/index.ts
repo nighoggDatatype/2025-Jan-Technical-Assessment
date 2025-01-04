874895cc67860a5b5dfd5649703a8f00557a8ca8
nighoggDatatype <hosherman1965@gmail.com>
2025-01-04 18:09:12 +0800

    Add simple landing page

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
+const path = require("path")
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (_req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (_req: any, res: { redirect: (arg0: string) => void; }) => {
+    res.redirect('/index.html');
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

e77120c9b86609ee534acfc25debc6ae02cae2a5
nighoggDatatype <hosherman1965@gmail.com>
2025-01-05 19:24:32 +0800

    Add middleware for form parsing

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Middleware to parse URL-encoded form data
+app.use(express.urlencoded({ extended: true })); 
+
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (_req: any, res: { redirect: (arg0: string) => void; }) => {

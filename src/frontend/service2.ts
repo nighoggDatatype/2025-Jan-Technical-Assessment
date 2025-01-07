df61ed525ebbb0bfce80dabca9a78792cced1c38
nighoggDatatype <hosherman1965@gmail.com>
2025-01-08 01:03:05 +0800

    Add pin visualization code

--- src/frontend/service2.ts
+++ src/frontend/service2.ts
@@ -11,3 +11,22 @@ L.tileLayer('https://www.onemap.gov.sg/maps/tiles/Default/{z}/{x}/{y}.png', {
     /** DO NOT REMOVE the OneMap attribution below **/
     attribution: '<img src="https://www.onemap.gov.sg/web-assets/images/logo/om_logo.png" style="height:20px;width:20px;"/>&nbsp;<a href="https://www.onemap.gov.sg/" target="_blank" rel="noopener noreferrer">OneMap</a>&nbsp;&copy;&nbsp;contributors&nbsp;&#124;&nbsp;<a href="https://www.sla.gov.sg/" target="_blank" rel="noopener noreferrer">Singapore Land Authority</a>'
 }).addTo(map)
+
+let selectedPin: null|L.Marker<any> = null
+const locationSelect = document.getElementById("location") as HTMLSelectElement;
+
+locationSelect.addEventListener("change", () => {
+    const selectedOption = locationSelect.options[locationSelect.selectedIndex];
+
+    if (selectedOption) {
+        if (selectedPin) {
+            map.removeLayer(selectedPin);
+        }
+        selectedPin = null
+        const lat = selectedOption.getAttribute("data-lat");
+        const long = selectedOption.getAttribute("data-long");
+        if (lat && long) {
+            selectedPin = L.marker([parseFloat(lat), parseFloat(long)]).addTo(map);
+        }
+    }
+});
